e662f6ae6549b49abd4304144681c977715c5796
ChamikaMuthukumarana <chamimuthukumarana1@gmail.com>
2019-05-21 01:36:44 +0530

    Add Features

---
+++ IT3030PAF2019_ShoppingCart(WE_7.2_G10)_IT17075954/shoppingCart/src/main/java/com/shoppingCart/app/ShoppingCartApplication.java
@@ -0,0 +1,22 @@
+package com.shoppingCart.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@SpringBootApplication
+public class ShoppingCartApplication {
+
+    public static void main(String[] args) {
+//    	 System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(ShoppingCartApplication.class, args);
+    }
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ShoppingCartApplication.class);
+}
+}
+
+
+
---
+++ IT3030PAF2019_ShoppingCart(WE_7.2_G10)_IT17075954/shoppingCart/src/test/java/com/shoppingCart1/app/SpringApplicationConfiguration.java
@@ -0,0 +1,56 @@
+//package com.shoppingCart1.app;
+//
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//import java.io.IOException;
+//import java.nio.charset.Charset;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.converter.HttpMessageConverter;
+//import org.springframework.mock.http.MockHttpOutputMessage;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//import com.shoppingCart.app.model.Customer;
+//
+//public @interface SpringApplicationConfiguration {
+//	
+//	private	Customer customer = new Customer();	
+//    private HttpMessageConverter<Object> mappingJackson2HttpMessageConverter;
+//    private MockMvc mockMvc;
+//	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+//	            MediaType.APPLICATION_JSON.getSubtype(),
+//	            Charset.forName("utf8"));
+//
+//	
+//	@Before
+//	public void setUp() {
+//		customer.setFirstName("Magalí");
+//		customer.setLastName("Kain");
+//		customer.setUsername("mkain");
+//		customer.setPassword("12345");
+//	}
+//	
+//	@Test
+//	public void addCustomer() {	
+//		try {
+//			String customerJson = json(customer);
+//			this.mockMvc.perform(post("/customers")
+//			            .contentType(contentType)
+//			            .content(customerJson))
+//			            .andExpect(status().isCreated());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+//
+//	protected String json(Object o) throws IOException {
+//		MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+//	    this.mappingJackson2HttpMessageConverter.write(
+//	            o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+//	    return mockHttpOutputMessage.getBodyAsString();
+//	}
+//
+//}
